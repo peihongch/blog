109ffaf7366e526b4bb3b1d44417f5713ab62395
steve <chph13420146901@gmail.com>
2019-02-26 20:29:50 +0800

    change the document title of the blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react';
 import ArticleList from '../components/ArticleList';
-import {Pagination} from "antd";
+import { Pagination } from 'antd';
 import styles from './index.css';
+
+document.title = '至繁归于至简@steve的博客';
 
 export default () => (
   <div>
     <ArticleList/>
-    <div className={styles["pagination-wrapper"]}>
+    <div className={styles['pagination-wrapper']}>
       <Pagination defaultCurrent={1} total={50} className={styles.pagination}/>
     </div>
   </div>
